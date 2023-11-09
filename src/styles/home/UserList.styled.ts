6e98b956612696d71553a1e8d4a8e907fb7dee4d
lilviolie <lilviolie@gmail.com>
2023-11-09 21:51:08 +0900

    Feat: 홈페이지 추가

---
+++ src/styles/home/UserList.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const List = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 80%;
+
+  padding: 10px;
+  overflow: auto;
+`;
+
+export default List;
